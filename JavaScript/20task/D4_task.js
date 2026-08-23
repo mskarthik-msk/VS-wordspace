8fbf5f8376a8a91d1da3823e9071a6e1b41ac418
mskarthik-msk <125405807+mskarthik-msk@users.noreply.github.com>
2026-08-24 04:00:02 +0530

    last

--- JavaScript/20task/D4_task.js
+++ JavaScript/20task/D4_task.js
@@ -64,7 +64,7 @@ console.log("------- Task 6. Nested Login")
 // let crtPass = "12345"
 // let otp = "5555";
 
-// let atempt = true;
+// let check = true;
 //     let inUser  = prompt("Username")
 //     let inPass  = prompt("Password")
 //     let inOTP  = prompt("OTP")
@@ -81,51 +81,285 @@ console.log("------- Task 6. Nested Login")
 //     }
 
 //T7
-console.log("------- Task ")
+console.log("------- Task 7. Number Pattern")
+for(let i=1; i<=10; i++)
+{console.log(i)}
+
+//T8
+console.log("------- Task 8. Multiplication Table")
+let table = 7
+for (let i=1; i<=10; i++){
+    console.log(`${table} x ${i} = ${table*i}`);
+}
+
+//T9
+console.log("------- Task 9. Reverse Countdown")
+let start = 10
+while(start>0){
+    console.log(start);
+    start--;
+}
+
+//T10
+console.log("------- Task 10. OTP System")
+
+let otp = 1234;
+let count = 3, done = false;
+while(count>0 && done == false){
+        let inOTP = 1234;
+        if(inOTP === otp){
+            console.log("OTP verified")
+            done=true;
+        }
+        else{
+            console.log("Incorrect OTP, Try again")
+            count--
+        }
+    }
+count == 0 ? console.log("Account blocked") : console.log("");
+
+//T11
+console.log("------- Task 11. Array Search")
+let fruits = ["apple", "banana", "orange", "grapes", "mango"];
+
+// every fruit
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);}
+
+// first fruit
+console.log("First fruit:", fruits[0]);
+
+// last fruit
+console.log("Last fruit:", fruits[fruits.length - 1]);
 
 
-//T
-console.log("------- Task ")
+//T12
+console.log("------- Task 12. Object Details")
+let employee = {
+    name: "Arun",
+    empId: "STK-101",
+    role: "Developer",
+    salary: 45000
+};
+
+for( let i in employee){
+    console.log(i,":",employee[i]);
+}
 
 
-//T
-console.log("------- Task ")
+//T13
+console.log("------- Task 13. Calculator Function")
+function calculator(a, b, operator) {
+    switch (operator) {
+        case "+": return a + b;
+        case "-": return a - b;
+        case "*": return a * b;
+        case "/": return a / b;
+        case "%": return a % b;
+        default : return "Invalid operator";
+    }
+}
+console.log(calculator(20, 5, "+"));
+console.log(calculator(20, 5, "*"));
+console.log(calculator(20, 5, "-"));
+console.log(calculator(20, 5, "/"));
+console.log(calculator(20, 5, "%"));
 
 
-//T
-console.log("------- Task ")
+//T14
+console.log("------- Task 14. Employee Salary")
+function salaryDetails(salary, bonus) {
+    return salary + bonus}
+console.log(salaryDetails(40000, 5000));
 
 
-//T
-console.log("------- Task ")
+//T15
+console.log("------- Task 15. Function With Default Parameter")
+function employeee(name, role = "Trainee") {
+    console.log(name);
+    console.log(role);
+}
+
+employeee("Arun");
+employeee("Kamal", "Developer");
 
 
-//T
-console.log("------- Task ")
+//T16
+console.log("------- Task 16. Callback Task")
+function calculate(a, b, callback) {return callback(a, b);}
+function add(a, b) {return a + b}
+function sub(a, b) {return a - b}
+function mul(a, b) {return a * b}
+
+console.log(calculate(10, 5, add));
+console.log(calculate(10, 5, sub));
+console.log(calculate(10, 5, mul));
 
 
-//T
-console.log("------- Task ")
+//T17
+console.log("------- Task 17. Generator Task")
+function* rewards(){
+    yield "₹100 Cashback";
+    yield "10% Discount";
+    yield "₹500 Cashback";
+    yield "20% Discount";
+    yield "Better Luck Next Time";
+}
+let reward = rewards();
+console.log(reward.next().value);
+console.log(reward.next().value);
+console.log(reward.next().value);
+console.log(reward.next().value);
+console.log(reward.next().value);
+console.log(reward.next().value);
+
+//T18
+console.log("------- Task 18. Spread Operator")
+let frontend = ["HTML", "CSS", "JavaScript"];
+let backend = ["Node", "Express", "MongoDB"];
+let fullStack = [...frontend,...backend]
+console.log(fullStack);
 
 
-//T
-console.log("------- Task ")
+//T19
+console.log("------- Task 19. Array Destructuring")
+let student = ["Arun", "ECE", 8.5, "Developer"];
+let [name, department, cgpa, role] = student;
+
+console.log("Name:", name);
+console.log("Department:", department);
+console.log("CGPA:", cgpa);
+console.log("Role:", role);
+
+//T20
+console.log("------- Task 20. Nested Object Destructuring")
+let company = {
+    name: "Stackly",
+    employee: {
+        name: "Arun",
+        role: "Developer",
+        salary: 50000
+    }
+};
+
+let {
+    employee: {
+        name: employeeName,
+        // role:
+        salary
+    }
+} = company;
+
+console.log(employeeName);
+console.log(role);
+console.log(salary);
 
 
-//T
-console.log("------- Task ")
+//Mini Project
+console.log("------- 🏆 Final Challenge — Mini Project");
+let students = [
+    {
+        name: "Arun",
+        department: "ECE",
+        mark: 85
+    },
+    {
+        name: "Kamal",
+        department: "CSE",
+        mark: 72
+    },
+    {
+        name: "Livin",
+        department: "ECE",
+        mark: 92
+    }
+];
 
 
-//T
-console.log("------- Task ")
+// 1. Print all students
+function displayStudents() {
+    for (let { name, department, mark } of students) {
+        console.log(name, department, mark);
+    }
+}
 
 
-//T
-console.log("------- Task ")
+// 2. Print only ECE students
+function displayECEStudents() {
+    for (let { name, department, mark } of students) {
+        if (department === "ECE") {
+            console.log(name, department, mark);
+        }
+    }
+}
 
 
-//T
-console.log("------- Task ")
+// 3. Find students who scored above 80
+function studentsAbove80() {
+    for (let { name, mark } of students) {
+        if (mark > 80) {
+            console.log(name, mark);
+        }
+    }
+}
 
 
+// 4. Calculate total marks
+function calculateTotal() {
+    let total = 0;
 
+    for (let { mark } of students) {
+        total += mark;
+    }
+
+    return total;
+}
+
+
+// 5. Calculate average
+function calculateAverage() {
+    let total = calculateTotal();
+    return total / students.length;
+}
+
+
+// 6. Display highest mark
+function highestMark() {
+    let highest = students[0].mark;
+
+    for (let { mark } of students) {
+        if (mark > highest) {
+            highest = mark;
+        }
+    }
+
+    return highest;
+}
+
+
+// 7. Display lowest mark
+function lowestMark() {
+    let lowest = students[0].mark;
+
+    for (let { mark } of students) {
+        if (mark < lowest) {
+            lowest = mark;
+        }
+    }
+
+    return lowest;
+}
+
+
+// Calling functions
+console.log("All Students:");
+displayStudents();
+console.log("ECE Students:");
+displayECEStudents();
+console.log("Students Above 80:");
+studentsAbove80();
+
+console.log("Total Marks:", calculateTotal());
+console.log("Average:", calculateAverage());
+console.log("Highest Mark:", highestMark());
+console.log("Lowest Mark:", lowestMark());
--- JavaScript/20task/D5_task.js
+++ JavaScript/20task/D5_task.js
@@ -28,7 +28,8 @@ console.log(`Power : ${a**b}`);
 console.log("------- Task Task 3: Age Checker")
 
 // let Vage= prompt("Enter Your Age");
-// Vage>=18 ? console.log("Eligible to vote"):console.log("Not eligible your Below 18");
+let Vage= 25;
+Vage>=18 ? console.log("Eligible to vote"):console.log("Not eligible your Below 18");
 
 
 //T4
@@ -51,41 +52,250 @@ console.log("------- Task Task 4: Student Grade")
 //T5
 console.log("------- Task Task 5: Login System")
 
-let crtUser = "admin";
-let crtPass = "12345"
+// let crtUser = "admin";
+// let crtPass = "12345"
 
-let atempt = true;
-    let inUser  = prompt("Username")
-    let inPass  = prompt("Password")
+// let atempt = true;
+//     let inUser  = prompt("Username")
+//     let inPass  = prompt("Password")
     
-    if(crtUser == inUser && crtPass == inPass){console.log("Login successful");}
-        if(crtUser != inUser && crtPass != inPass){console.log("Invalid credentials");}
-            if(crtUser !== inUser){console.log("Incorrect Username");}
-                if(crtPass !== inPass){console.log("Incorrect Password");}
+//     if(crtUser == inUser && crtPass == inPass){console.log("Login successful");}
+//         if(crtUser != inUser && crtPass != inPass){console.log("Invalid credentials");}
+//             if(crtUser !== inUser && crtPass == inPass){console.log("Incorrect Username");}
+//                 if(crtPass !== inPass && crtUser == inUser){console.log("Incorrect Password");}
     
 
-//T
-console.log("------- Task ")
+//T6
+console.log("------- Task 6: ATM Withdrawal")
+
+// let balance = 10000;
+// let withdrawal = prompt("Withdrawal Amount");
+// let remain = balance-withdrawal;
+// if(100>withdrawal){
+//     alert("Enter valid Amount")}
+//     else if(balance<withdrawal){
+//         alert("Your balance is Low")}
+//         else if(withdrawal%100 != 0){
+//             alert("Withdrawal amount should be a multiple of 100");}
+//             else{
+//                 alert(`₹${withdrawal} Withdrawal successful, \nRemaining balance: ₹${remain}`)
+//             }
+
+//T7
+console.log("------- Task 7: Multiplication Table")
+
+let table = Number (prompt("Input the Number"))
+for (let i=1; i<=10; i++){
+    console.log(`${table} x ${i} = ${table*i}`);
+}
+
+//T8
+console.log("------- Task 8: Reverse Number")
+
+let abc= 12345;
+let cat=0, dog=0
+// for (let index = 1; 0 < abc; index++)
+while(abc>0)
+    {
+    cat= abc%10;
+    dog= dog*10 + cat;
+    abc=Math.floor(abc/10);
+}
+console.log(dog);
+
+//T9
+console.log("------- Task 9: OTP System")
+
+let otp = 5555;
+let count = 3, done = false;
+while(count>0 && done == false){
+        let inOTP = Number (prompt("OTP"))
+        if(inOTP === otp){
+            console.log("OTP verified")
+            done=true;
+        }
+        else{
+            console.log("Incorrect OTP, Try again")
+            count--
+        }
+    }
+count == 0 ? console.log("Account blocked") : console.log("");
+        
+//T10
+console.log("------- Task 10: Salary Calculator")
+
+function calculateSalary(basicSalary, bonus) {
+    return basicSalary + bonus;
+}
+
+console.log(calculateSalary(30000, 5000));
+
+//T11
+console.log("------- Task 11: Calculator Function")
+
+function add(a, b) {return a + b;}
+console.log(add(10, 5));
+
+function subtract(a, b) {return a - b;}
+console.log(subtract(20, 12));
+
+function multiply(a, b) {return a * b;}
+console.log(multiply(7, 4));
+
+function divide(a, b) {return a / b;}
+console.log(divide(33, 3));
+
+//T12
+console.log("------- Task 12: Eligibility Function")
+
+let JAge=25
+let JHeight=167
+let JWeight=72
+if(21<=JAge)
+    {if(170 <= JHeight)
+        {if (70 <= JWeight) 
+            {console.log("Eligible")}
+        else{console.log("Not eligible")}
+    }else{console.log("Not eligible")}
+}else{console.log("Not eligible")}
+
+//T13
+console.log("------- Task 13: Shopping Cart")
+
+let cart = ["Laptop", "Mouse", "Keyboard"];
+let remItem = cart.indexOf("Mouse");
+//1
+cart.push("Monitor");
+//2
+cart.splice(remItem, 1);
+//3
+cart.unshift("Headset");
+//4
+cart.pop();
+//5
+console.log(cart);
+
+//T14
+console.log("------- Task 14: Find Maximum")
+
+let numbers = [10, 45, 23, 89, 12, 67];
+let chech  = numbers[0]
+for(let i=0; i<numbers.length; i++){
+    if(numbers[i]>chech){
+        chech= numbers[i]
+    }        
+}
+console.log(chech);
+
+//T15
+console.log("------- Task 15: Remove Duplicate Values")
+
+let numberrs = [1, 2, 3, 2, 4, 1, 5];
+let unique = [];
+for (let i = 0; i < numberrs.length; i++) {
+    if (!unique.includes(numberrs[i])) {
+        unique.push(numberrs[i]);
+    }
+}
+console.log(unique);
+
+//T16
+console.log("------- Task 16: Employee Salary Filter")
+
+let employes = [
+  {name: "Arun", salary: 30000},
+  {name: "Bala", salary: 50000},
+  {name: "Kumar", salary: 25000},
+  {name: "Ravi", salary: 70000}
+];
+
+console.log(employes.filter(e=>e.salary>=40000));
 
 
-//T
-console.log("------- Task ")
+//T17
+console.log("------- Task 17: Increase Salary")
+
+let increase= employes.map(e=>{
+    if(e.salary < 40000){
+        e.salary = e.salary+5000
+    }else{
+        e.salary += 10000
+    }return e;
+}
+)
+console.log(increase);
+
+//T18
+console.log("------- Task 18: Total Salary")
+
+console.log(employes.reduce((total, sale) => {
+    return total+sale.salary},0));
+
+//T19
+console.log("------- Task 19: Check Employee")
+
+console.log(employes.some(e=>{e.salary>100000}));
+console.log(employes.every(e=>{e.salary>=20000}));
 
 
-//T
-console.log("------- Task ")
+//T20
+console.log("------- Task 20: Employee Management Mini Program")
 
+let employees = [
+    {
+        id: 101,
+        name: "Arun",
+        department: "IT",
+        salary: 35000
+    },
+    {
+        id: 102,
+        name: "Bala",
+        department: "HR",
+        salary: 45000
+    },
+    {
+        id: 103,
+        name: "Kumar",
+        department: "IT",
+        salary: 60000
+    }
+];
 
-//T
-console.log("------- Task ")
+// 1
+employees.forEach(employee => {console.log(employee.name);});
 
+// 2
+console.log(employees.filter(employee => employee.department === "IT"));
 
-//T
-console.log("------- Task ")
+// 3
+let increasedSalary = employees.map(employee => {
+    return {...employee,
+        salary: employee.salary * 1.10}});
+console.log(increasedSalary);
 
+// 4
+console.log(employees.find(employee => employee.salary === 45000));
 
-//T
-console.log("------- Task ")
+// 5
+let totalSalary = employees.reduce((total, employee) => {
+    return total + employee.salary;
+}, 0);
 
+console.log(totalSalary);
 
+// 6
+console.log(employees.some(employee => employee.salary > 50000));
 
+// 7
+console.log(employees.every(employee => employee.salary > 20000));
+
+// 8
+console.log([...employees].sort((a, b) => b.salary - a.salary));
+
+// 9
+employees.forEach(employee => {let { name, salary } = employee;
+    console.log(name, salary)});
+
+// 10
+console.log([...employees]);
